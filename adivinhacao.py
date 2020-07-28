af2f9c8de5b9568caec4c5d5c667ae1736fb6993
Gustavo <luisgustavocantisani@gmail.com>
2020-07-28 18:31:39 -0300

    Adicionando maior número de tentativas

--- adivinhacao.py
+++ adivinhacao.py
@@ -3,22 +3,27 @@ print("Bem vindo ao jogo de adivinhação!")
 print("*******************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
 
-chute = int(input("Digite o seu número: "))
+while(rodada <= total_de_tentativas):
+    print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+    chute = int(input("Digite o seu número: "))
 
-print("Você digitou", chute)
+    print("Você digitou", chute)
 
-acertou = chute == numero_secreto
-maior = chute > numero_secreto
-menor = chute < numero_secreto
+    acertou = chute == numero_secreto
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
 
-if (acertou):
-    print("Você acertou!")
+    if (acertou):
+        print("Você acertou!")
 
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior do que o número secreto.")
-    elif(menor):
-        print("Você errou! O seu chute foi menor do que o número secreto.")
+    else:
+        if(maior):
+            print("Você errou! O seu chute foi maior do que o número secreto.")
+        elif(menor):
+            print("Você errou! O seu chute foi menor do que o número secreto.")
 
+    rodada = rodada + 1
 print("Fim do jogo")
