6fe513770b683613cd1c9a65410d9a1f8a090f3a
Gustavo <luisgustavocantisani@gmail.com>
2020-07-28 15:51:59 -0300

    Primeiras linhas de um jogo de adivinhação

---
+++ adivinhacao.py
@@ -0,0 +1,17 @@
+print("*******************************")
+print("Bem vindo ao jogo de adivinhação!")
+print("*******************************")
+
+numero_secreto = 42
+
+chute = int(input("Digite o seu número: "))
+
+print("Você digitou", chute)
+
+if (numero_secreto == chute):
+    print("Você acertou!")
+
+else:
+    print("Você errou!")
+
+print("Fim do jogo")
