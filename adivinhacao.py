b5ae29d550f62bad223eb53fb2ff4569dd66a910
Gustavo <luisgustavocantisani@gmail.com>
2020-07-28 16:10:04 -0300

    Melhorando a legibilidade do código

--- adivinhacao.py
+++ adivinhacao.py
@@ -8,10 +8,17 @@ chute = int(input("Digite o seu número: "))
 
 print("Você digitou", chute)
 
-if (numero_secreto == chute):
+acertou = chute == numero_secreto
+maior = chute > numero_secreto
+menor = chute < numero_secreto
+
+if (acertou):
     print("Você acertou!")
 
 else:
-    print("Você errou!")
+    if(maior):
+        print("Você errou! O seu chute foi maior do que o número secreto.")
+    elif(menor):
+        print("Você errou! O seu chute foi menor do que o número secreto.")
 
 print("Fim do jogo")
