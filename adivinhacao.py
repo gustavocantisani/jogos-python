9f89ec9e0de7ae36f9dd82e6308b4c74bdb08ec4
Gustavo <luisgustavocantisani@gmail.com>
2020-08-01 21:03:23 -0300

    Utilizando o for

--- adivinhacao.py
+++ adivinhacao.py
@@ -4,13 +4,17 @@ print("*******************************")
 
 numero_secreto = 42
 total_de_tentativas = 3
-rodada = 1
 
-while(rodada <= total_de_tentativas):
+
+for rodada in range(1, total_de_tentativas + 1):
     print("Tentativa {} de {}".format(rodada, total_de_tentativas))
-    chute = int(input("Digite o seu número: "))
+    chute = int(input("Digite um número entre 1 e 100: "))
 
     print("Você digitou", chute)
+
+    if(chute < 1 or chute > 100):
+        print("Você deve digitar um número entre 1 e 100!")
+        continue
 
     acertou = chute == numero_secreto
     maior = chute > numero_secreto
@@ -18,6 +22,7 @@ while(rodada <= total_de_tentativas):
 
     if (acertou):
         print("Você acertou!")
+        break
 
     else:
         if(maior):
@@ -25,5 +30,4 @@ while(rodada <= total_de_tentativas):
         elif(menor):
             print("Você errou! O seu chute foi menor do que o número secreto.")
 
-    rodada = rodada + 1
 print("Fim do jogo")
