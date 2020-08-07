4d2b006ed014d28540c658bf072f701d8a31fd0a
Gustavo <luisgustavocantisani@gmail.com>
2020-08-07 12:01:25 -0300

    Adicionando loop

--- forca.py
+++ forca.py
@@ -4,7 +4,17 @@ def jogar():
     print("Bem vindo ao jogo da forca!")
     print("*******************************")
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("jogando ...")
+
+
     print("Fim do jogo")
 
 if(__name__ == "__main__"):
     jogar()
+
+
