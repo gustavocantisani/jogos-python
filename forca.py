1e534a40970b4b2c7f827af27cd371347e6b45b1
Gustavo <luisgustavocantisani@gmail.com>
2020-08-10 15:19:41 -0300

    Adicionando entrada do usuário e outras funções

--- forca.py
+++ forca.py
@@ -9,8 +9,17 @@ def jogar():
     acertou = False
 
     while(not enforcou and not acertou):
+
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
         print("jogando ...")
 
+        index = 0
+        for letra in palavra_secreta:
+            if(chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posição {}".format(letra, index))
+            index = index + 1
 
     print("Fim do jogo")
 
