5c4a5cc92751b716f2194a2cc35cf47343ea4e03
Gustavo <luisgustavocantisani@gmail.com>
2020-08-15 14:53:56 -0300

    Adicionando fim de jogo através do fim de um loop

--- forca.py
+++ forca.py
@@ -4,22 +4,39 @@ def jogar():
     print("Bem vindo ao jogo da forca!")
     print("*******************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
         print("jogando ...")
 
-        index = 0
-        for letra in palavra_secreta:
-            if(chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posição {}".format(letra, index))
-            index = index + 1
+        if(chute in palavra_secreta):
+            posicao = 0
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    letras_acertadas[posicao] = letra
+                posicao += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if(acertou):
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
 
     print("Fim do jogo")
 
